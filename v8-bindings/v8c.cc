2300ec6274ade469485c78beed41fb65ed777778
Viatcheslav Gachkaylo <vgachkaylo@crystalnix.com>
2011-02-17 16:13:16 +0600

    Added basic Value-derived types support in C bindings

--- v8-bindings/v8c.cc
+++ v8-bindings/v8c.cc
@@ -33,6 +33,22 @@ void v8_handle_scope_free(V8CHandleScope* hs) {
   delete hs;
 }
 
+V8Handle v8_handle_scope_close(V8CHandleScope *self, V8Handle value, V8HandleType handle_type) {
+  V8Handle res = (V8Handle)0;
+  
+  switch (handle_type) {
+  case V8HT_VALUE:
+  default:
+    res = unwrap_handle(self->Close(wrap_handle<v8::Value>(value)));
+  }
+  
+  return res;
+}
+
+int v8_handle_scope_number_of_handles() {
+  return v8::HandleScope::NumberOfHandles();
+}
+
 V8Handle v8_script_compile(V8Handle code) {
   return unwrap_handle(v8::Script::Compile(wrap_handle<v8::String>(code)));
 }
@@ -41,12 +57,170 @@ V8Handle v8_script_run(V8Handle script) {
   return unwrap_handle(wrap_handle<v8::Script>(script)->Run());
 }
 
+/* Value */
+bool v8_value_is_undefined  (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsUndefined();
+}
+
+bool v8_value_is_null       (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsNull();
+}
+
+bool v8_value_is_true       (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsTrue();
+}
+
+bool v8_value_is_false      (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsFalse();
+}
+
+bool v8_value_is_string     (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsString();
+}
+
+bool v8_value_is_function   (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsFunction();
+}
+
+bool v8_value_is_array      (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsArray();
+}
+
+bool v8_value_is_object     (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsObject();
+}
+
+bool v8_value_is_boolean    (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsBoolean();
+}
+
+bool v8_value_is_number     (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsNumber();  
+}
+
+bool v8_value_is_external   (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsExternal();
+}
+
+bool v8_value_is_int32      (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsInt32();
+}
+
+bool v8_value_is_uint32     (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsUint32();
+}
+
+bool v8_value_is_date       (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsDate();
+}
+
+bool v8_value_is_regexp     (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IsRegExp();
+}
+
+V8Handle v8_value_to_boolean    (V8Handle self) {
+  return unwrap_handle(wrap_handle<v8::Value>(self)->ToBoolean());
+}
+
+V8Handle v8_value_to_number     (V8Handle self) {
+  return unwrap_handle(wrap_handle<v8::Value>(self)->ToNumber());
+}
+
+V8Handle v8_value_to_string     (V8Handle self) {
+  return unwrap_handle(wrap_handle<v8::Value>(self)->ToString());
+}
+
+V8Handle v8_value_to_detail_string (V8Handle self) {
+  return unwrap_handle(wrap_handle<v8::Value>(self)->ToDetailString());
+}
+
+V8Handle v8_value_to_object     (V8Handle self) {
+  return unwrap_handle(wrap_handle<v8::Value>(self)->ToObject());
+}
+
+V8Handle v8_value_to_integer    (V8Handle self) {
+  return unwrap_handle(wrap_handle<v8::Value>(self)->ToInteger());
+}
+
+V8Handle v8_value_to_uint32     (V8Handle self) {
+  return unwrap_handle(wrap_handle<v8::Value>(self)->ToUint32());
+}
+
+V8Handle v8_value_to_int32      (V8Handle self) {
+  return unwrap_handle(wrap_handle<v8::Value>(self)->ToInt32());
+}
+
+V8Handle v8_value_to_external   (V8Handle self) {
+  return unwrap_handle(v8::Handle<v8::External>::Cast(wrap_handle<v8::Value>(self)));
+}
+
+V8Handle v8_value_to_array_index(V8Handle self) {
+  return unwrap_handle(wrap_handle<v8::Value>(self)->ToArrayIndex());
+}
+
+bool     v8_value_boolean_value (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->BooleanValue();
+}
+
+double   v8_value_number_value  (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->NumberValue();
+}
+
+int64_t  v8_value_integer_value (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->IntegerValue();
+}
+
+uint32_t v8_value_uint32_value  (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->Uint32Value();
+}
+
+int32_t  v8_value_int32_value   (V8Handle self) {
+  return wrap_handle<v8::Value>(self)->Int32Value();
+}
+
+void*    v8_value_voidp_value   (V8Handle self) {
+  return v8::Handle<v8::External>::Cast(wrap_handle<v8::Value>(self))->Value();
+}
+
+bool v8_value_equals(V8Handle self, V8Handle that) {
+  return wrap_handle<v8::Value>(self)->Equals(wrap_handle<v8::Value>(that));
+}
+
+bool v8_value_strict_equals(V8Handle self, V8Handle that) {
+  return wrap_handle<v8::Value>(self)->StrictEquals(wrap_handle<v8::Value>(that));
+}
+
+/* Boolean */
+bool v8_boolean_value(V8Handle self) {
+  return wrap_handle<v8::Boolean>(self)->Value();
+}
+
+V8Handle v8_boolean_new(bool value) {
+  return unwrap_handle(v8::Boolean::New(value));
+}
+
 V8Handle v8_string_new_utf8(const char* data, int length) {
   return unwrap_handle(v8::String::New(data, length));
 }
 
 int v8_string_length(V8Handle h) {
   return wrap_handle<v8::String>(h)->Length();
+}
+
+int         v8_string_utf8_length   (V8Handle self) {
+  return wrap_handle<v8::String>(self)->Utf8Length();
+}
+
+V8Handle    v8_string_empty         (void) {
+  return unwrap_handle(v8::String::Empty());
+}
+
+bool        v8_string_is_external   (V8Handle self) {
+  return wrap_handle<v8::String>(self)->IsExternal();
+}
+
+bool        v8_string_is_external_ascii   (V8Handle self) {
+  return wrap_handle<v8::String>(self)->IsExternalAscii();
 }
 
 V8StringUtf8Value* v8_string_utf8_value_new(V8Handle handle) {
@@ -63,6 +237,94 @@ char* v8_string_utf8_value_chars(V8StringUtf8Value* utf8) {
 
 void v8_string_utf8_value_free(V8StringUtf8Value* utf8) {
   delete utf8;
+}
+
+/* String::AsciiValue */
+V8StringAsciiValue*     v8_string_ascii_value_new(V8Handle handle) {
+  return new v8::String::AsciiValue(wrap_handle<v8::Value>(handle));
+}
+
+int     v8_string_ascii_value_length (V8StringAsciiValue* self) {
+  return self->length();
+}
+
+char*   v8_string_ascii_value_chars  (V8StringAsciiValue* self) {
+  return **self;
+}
+
+void    v8_string_ascii_value_free   (V8StringAsciiValue* self) {
+  delete self;
+}
+
+/* String::Value (2-byte string) */
+V8StringValue*     v8_string_value_new(V8Handle handle) {
+  return new v8::String::Value(wrap_handle<v8::Value>(handle));
+}
+
+int         v8_string_value_length (V8StringValue* self) {
+  return self->length();
+}
+
+uint16_t*   v8_string_value_chars  (V8StringValue* self) {
+  return **self;
+}
+
+void        v8_string_value_free   (V8StringValue* self) {
+  delete self;
+}
+
+/* Number */
+double v8_number_value(V8Handle self) {
+  return wrap_handle<v8::Number>(self)->Value(); 
+}
+
+V8Handle v8_number_new(double value) {
+  return unwrap_handle(v8::Number::New(value));
+}
+
+/* Integer */
+V8Handle v8_integer_new(int32_t value) {
+  return unwrap_handle(v8::Integer::New(value));
+}
+
+V8Handle v8_integer_new_from_unsigned(uint32_t value) {
+  return unwrap_handle(v8::Integer::NewFromUnsigned(value));
+}
+
+int64_t v8_integer_value(V8Handle self) {
+  return wrap_handle<v8::Integer>(self)->Value(); 
+}
+
+/* Int32 */
+int32_t v8_int32_value(V8Handle self) {
+  return wrap_handle<v8::Int32>(self)->Value(); 
+}
+
+/* Uint32 */
+uint32_t v8_uint32_value(V8Handle self) {
+  return wrap_handle<v8::Uint32>(self)->Value(); 
+}
+
+/* Date */
+V8Handle    v8_date_new(double time) {
+  return unwrap_handle(v8::Date::New(time));
+}
+
+double      v8_date_number_value(V8Handle self) {
+  return wrap_handle<v8::Date>(self)->NumberValue();
+}
+
+void        v8_date_time_configuration_change_notification(void) {
+  v8::Date::DateTimeConfigurationChangeNotification();
+}
+
+/* External */
+V8Handle    v8_external_new(void *value) {
+  return unwrap_handle(v8::External::New(value));
+}
+
+void*       v8_external_value(V8Handle self) {
+  return wrap_handle<v8::External>(self)->Value();  
 }
 
 void v8_template_set(V8Handle tmpl, V8Handle name, V8Handle value) {
@@ -137,7 +399,7 @@ void v8_function_template_set_class_name(V8Handle self, V8Handle name)
   wrap_handle<v8::FunctionTemplate>(self)->SetClassName(wrap_handle<v8::String>(name));
 }
 
-void v8_function_template_set_hidden_prototype(bool value)
+void v8_function_template_set_hidden_prototype(V8Handle self, bool value)
 {
   wrap_handle<v8::FunctionTemplate>(self)->SetHiddenPrototype(value);
 }
@@ -157,7 +419,7 @@ V8Handle v8_object_template_new_instance(V8Handle self) {
 
 /* getter / setter wrappers for object template */
 
-v8::Handle<Value> accessor_getter_callback(v8::Local<v8::String> property, const v8::AccessorInfo& info) {
+v8::Handle<v8::Value> accessor_getter_callback(v8::Local<v8::String> property, const v8::AccessorInfo& info) {
   v8::Local<v8::External> data = v8::Local<v8::External>::Cast(info.Data());
   V8AccessorData *accessor_data =
       reinterpret_cast<V8AccessorData*>(data->Value());
@@ -169,12 +431,12 @@ void accessor_setter_callback(v8::Local<v8::String> property, v8::Local<v8::Valu
                              const v8::AccessorInfo& info) 
 {
   v8::Local<v8::External> data = v8::Local<v8::External>::Cast(info.Data());
-  V8AccessorData *named_data =
+  V8AccessorData *accessor_data =
       reinterpret_cast<V8AccessorData*>(data->Value());
   accessor_data->setter(unwrap_handle(property), unwrap_handle(value), &info);
 }
 
-v8::Handle<Value> named_getter_callback(v8::Local<v8::String> property, const v8::AccessorInfo& info) {
+v8::Handle<v8::Value> named_getter_callback(v8::Local<v8::String> property, const v8::AccessorInfo& info) {
   v8::Local<v8::External> data = v8::Local<v8::External>::Cast(info.Data());
   V8NamedPropertyData *named_data =
       reinterpret_cast<V8NamedPropertyData*>(data->Value());
@@ -182,16 +444,20 @@ v8::Handle<Value> named_getter_callback(v8::Local<v8::String> property, const v8
   return wrap_handle<v8::Value>(named_data->getter(unwrap_handle(property), &info));
 }
 
-void named_setter_callback(v8::Local<v8::String> property, v8::Local<v8::Value> value, 
+v8::Handle<v8::Value> named_setter_callback(v8::Local<v8::String> property, v8::Local<v8::Value> value, 
                              const v8::AccessorInfo& info) 
 {
   v8::Local<v8::External> data = v8::Local<v8::External>::Cast(info.Data());
   V8NamedPropertyData *named_data =
       reinterpret_cast<V8NamedPropertyData*>(data->Value());
-  named_data->setter(unwrap_handle(property), unwrap_handle(value), &info);
+  return wrap_handle<v8::Value>(named_data->setter(
+                                  unwrap_handle(property), 
+                                  unwrap_handle(value), 
+                                  &info)
+                                );
 }
 
-v8::Handle<Value> indexed_getter_callback(uint32_t index, const v8::AccessorInfo& info) {
+v8::Handle<v8::Value> indexed_getter_callback(uint32_t index, const v8::AccessorInfo& info) {
   v8::Local<v8::External> data = v8::Local<v8::External>::Cast(info.Data());
   V8IndexedPropertyData *indexed_data =
       reinterpret_cast<V8IndexedPropertyData*>(data->Value());
@@ -199,21 +465,25 @@ v8::Handle<Value> indexed_getter_callback(uint32_t index, const v8::AccessorInfo
   return wrap_handle<v8::Value>(indexed_data->getter(index, &info));
 }
 
-void indexed_setter_callback(uint32_t index, v8::Local<v8::Value> value, 
+v8::Handle<v8::Value> indexed_setter_callback(uint32_t index, v8::Local<v8::Value> value, 
                              const v8::AccessorInfo& info) 
 {
   v8::Local<v8::External> data = v8::Local<v8::External>::Cast(info.Data());
   V8IndexedPropertyData *indexed_data =
       reinterpret_cast<V8IndexedPropertyData*>(data->Value());
-  indexed_data->setter(index, unwrap_handle(value), &info);
+  return wrap_handle<v8::Value>(indexed_data->setter(
+                                  index, 
+                                  unwrap_handle(value), 
+                                  &info)
+                                );
 }
 
 void v8_object_template_set_accessor(V8Handle self, V8Handle name, 
                                      V8AccessorData *accessor_data) {
   wrap_handle<v8::ObjectTemplate>(self)->SetAccessor(
       wrap_handle<v8::String>(name),
-      generic_getter_callback,
-      setter != NULL ? generic_setter_callback : 0,
+      accessor_getter_callback,
+      (accessor_data->setter != NULL) ? accessor_setter_callback : 0,
       v8::External::New(accessor_data)      
   );
 }
@@ -221,19 +491,18 @@ void v8_object_template_set_accessor(V8Handle self, V8Handle name,
 void v8_object_template_set_named_property_handler(V8Handle self,
                                                    V8NamedPropertyData *named_property_data) {
    wrap_handle<v8::ObjectTemplate>(self)->SetNamedPropertyHandler(
-       generic_getter_callback,
-       setter != NULL ? generic_setter_callback : 0,
+       named_getter_callback,
+       (named_property_data->setter != NULL) ? named_setter_callback : 0,
        0, 0, 0,
        v8::External::New(named_property_data)      
-   );
-                                                     
+   );                                                  
 }
 
 void v8_object_template_set_indexed_property_handler(V8Handle self,
                                                      V8IndexedPropertyData *indexed_property_data) {
    wrap_handle<v8::ObjectTemplate>(self)->SetIndexedPropertyHandler(
        indexed_getter_callback,
-       setter != NULL ? indexed_setter_callback : 0,
+       (indexed_property_data->setter != NULL) ? indexed_setter_callback : 0,
        0, 0, 0,
        v8::External::New(indexed_property_data)      
    );
--- v8-bindings/v8c.h
+++ v8-bindings/v8c.h
@@ -44,6 +44,35 @@
 */
 typedef void* V8Handle;
 
+typedef enum _V8HandleType {
+    V8HT_UNKNOWN = 0,
+    V8HT_DATA,
+    V8HT_SCRIPT,
+    V8HT_MESSAGE,
+    V8HT_STACK_TRACE,
+    V8HT_STACK_FRAME,
+    V8HT_VALUE,
+    V8HT_PRIMITIVE,
+    V8HT_BOOLEAN,
+    V8HT_STRING,
+    V8HT_NUMBER,
+    V8HT_INTEGER,
+    V8HT_INT32,
+    V8HT_UINT32,
+    V8HT_DATE,
+    V8HT_REGEXP,
+    V8HT_OBJECT,
+    V8HT_ARRAY,
+    V8HT_FUNCTION,
+    V8HT_EXTERNAL,
+    V8HT_TEMPLATE,
+    V8HT_FUNCTION_TEMPLATE,
+    V8HT_OBJECT_TEMPLATE,
+    V8HT_SIGNATURE,
+    V8HT_TYPE_SWITCH,
+    V8HT_CONTEXT    
+} V8HandleType;
+
 /* To have the compiler help catch type errors, this file is #included by
    both the C++ implementation and the C user.  These typedefs make the
    function prototypes work on both sides.
@@ -51,41 +80,133 @@ typedef void* V8Handle;
 #ifdef __cplusplus
 class V8CHandleScope;
 extern "C" {
-typedef V8CHandleScope V8HandleScope;
-typedef v8::Arguments V8Arguments;
-typedef v8::AccessorInfo V8AccessorInfo;
+typedef V8CHandleScope          V8HandleScope;
+typedef v8::Arguments           V8Arguments;
+typedef v8::AccessorInfo        V8AccessorInfo;
 typedef v8::ExtensionConfiguration* V8ExtensionConfiguration;
-typedef v8::String::Utf8Value V8StringUtf8Value;
-typedef v8::TryCatch V8TryCatch;
+typedef v8::String::Utf8Value   V8StringUtf8Value;
+typedef v8::String::AsciiValue  V8StringAsciiValue;
+typedef v8::String::Value       V8StringValue;
+typedef v8::TryCatch            V8TryCatch;
 #else  /* Compiling on the C side. */
 typedef int bool;
-typedef struct _V8HandleScope V8HandleScope;
-typedef struct _V8Arguments V8Arguments;
-typedef struct _V8AccessorInfo V8AccessorInfo;
-typedef void* V8ExtensionConfiguration;
-typedef void V8StringUtf8Value;
-typedef struct _V8TryCatch V8TryCatch;
+typedef struct _V8HandleScope   V8HandleScope;
+typedef struct _V8Arguments     V8Arguments;
+typedef struct _V8AccessorInfo  V8AccessorInfo;
+typedef void*                   V8ExtensionConfiguration;
+typedef void                    V8StringUtf8Value;
+typedef void                    V8StringAsciiValue;
+typedef void                    V8StringValue;
+typedef struct _V8TryCatch      V8TryCatch;
 #endif
 
 /* Handle */
-bool v8_handle_is_empty(V8Handle handle);
+bool        v8_handle_is_empty(V8Handle handle);
 
 /* HandleScope */
 V8HandleScope* v8_handle_scope_new();
 void           v8_handle_scope_free(V8HandleScope* hs);
+V8Handle       v8_handle_scope_close(V8HandleScope *self, V8Handle value, V8HandleType handle_type);
+int            v8_handle_scope_number_of_handles();
 
 /* Script */
 V8Handle v8_script_compile(V8Handle source);
 V8Handle v8_script_run(V8Handle script);
 
+/* Value */
+bool v8_value_is_undefined  (V8Handle self);
+bool v8_value_is_null       (V8Handle self);
+bool v8_value_is_true       (V8Handle self);
+bool v8_value_is_false      (V8Handle self);
+bool v8_value_is_string     (V8Handle self);
+bool v8_value_is_function   (V8Handle self);
+bool v8_value_is_array      (V8Handle self);
+bool v8_value_is_object     (V8Handle self);
+bool v8_value_is_boolean    (V8Handle self);
+bool v8_value_is_number     (V8Handle self);
+bool v8_value_is_external   (V8Handle self);
+bool v8_value_is_int32      (V8Handle self);
+bool v8_value_is_uint32     (V8Handle self);
+bool v8_value_is_date       (V8Handle self);
+bool v8_value_is_regexp     (V8Handle self);
+
+V8Handle v8_value_to_boolean    (V8Handle self);
+V8Handle v8_value_to_number     (V8Handle self);
+V8Handle v8_value_to_string     (V8Handle self);
+V8Handle v8_value_to_detail_string (V8Handle self);
+V8Handle v8_value_to_object     (V8Handle self);
+V8Handle v8_value_to_integer    (V8Handle self);
+V8Handle v8_value_to_uint32     (V8Handle self);
+V8Handle v8_value_to_int32      (V8Handle self);
+
+V8Handle v8_value_to_external   (V8Handle self);
+
+V8Handle v8_value_to_array_index(V8Handle self);
+
+bool     v8_value_boolean_value (V8Handle self);
+double   v8_value_number_value  (V8Handle self);
+int64_t  v8_value_integer_value (V8Handle self);
+uint32_t v8_value_uint32_value  (V8Handle self);
+int32_t  v8_value_int32_value   (V8Handle self);
+
+void*    v8_value_voidp_value   (V8Handle self);
+
+bool v8_value_equals(V8Handle self, V8Handle that);
+bool v8_value_strict_equals(V8Handle self, V8Handle that);
+
+/* Boolean */
+bool v8_boolean_value(V8Handle self);
+V8Handle v8_boolean_new(bool value);
+
 /* String */
-V8Handle v8_string_new_utf8(const char* data, int length);
+V8Handle    v8_string_new_utf8      (const char* data, int length);
+int         v8_string_length        (V8Handle self);
+int         v8_string_utf8_length   (V8Handle self);
+V8Handle    v8_string_empty         (void);
+bool        v8_string_is_external   (V8Handle self);
+bool        v8_string_is_external_ascii   (V8Handle self);
 
 /* String::Utf8Value */
 V8StringUtf8Value* v8_string_utf8_value_new(V8Handle handle);
-int v8_string_utf8_value_length(V8StringUtf8Value* utf8);
-char* v8_string_utf8_value_chars(V8StringUtf8Value* utf8);
-void v8_string_utf8_value_free(V8StringUtf8Value* utf8);
+int     v8_string_utf8_value_length(V8StringUtf8Value* utf8);
+char*   v8_string_utf8_value_chars(V8StringUtf8Value* utf8);
+void    v8_string_utf8_value_free(V8StringUtf8Value* utf8);
+
+/* String::AsciiValue */
+V8StringAsciiValue*     v8_string_ascii_value_new(V8Handle handle);
+int     v8_string_ascii_value_length (V8StringAsciiValue* self);
+char*   v8_string_ascii_value_chars  (V8StringAsciiValue* self);
+void    v8_string_ascii_value_free   (V8StringAsciiValue* self);
+
+/* String::Value (2-byte string) */
+V8StringValue*     v8_string_value_new(V8Handle handle);
+int         v8_string_value_length (V8StringValue* self);
+uint16_t*   v8_string_value_chars  (V8StringValue* self);
+void        v8_string_value_free   (V8StringValue* self);
+
+/* Number */
+double v8_number_value(V8Handle self);
+V8Handle v8_number_new(double value);
+
+/* Integer */
+V8Handle v8_integer_new(int32_t value);
+V8Handle v8_integer_new_from_unsigned(uint32_t value);
+int64_t v8_integer_value(V8Handle self);
+
+/* Int32 */
+int32_t v8_int32_value(V8Handle self);
+
+/* Uint32 */
+uint32_t v8_uint32_value(V8Handle self);
+
+/* Date */
+V8Handle    v8_date_new(double time);
+double      v8_date_number_value(V8Handle self);
+void        v8_date_time_configuration_change_notification(void);
+
+/* External */
+V8Handle    v8_external_new(void *value);
+void*       v8_external_value(V8Handle self);
 
 /* Template */
 void v8_template_set(V8Handle tmpl, V8Handle name, V8Handle value);
@@ -115,14 +236,14 @@ V8Handle v8_function_template_instance_template(V8Handle self);
 void v8_function_template_inherit(V8Handle self, V8Handle parent);
 V8Handle v8_function_template_prototype_template(V8Handle self);
 void v8_function_template_set_class_name(V8Handle self, V8Handle name);
-void v8_function_template_set_hidden_prototype(bool value);
+void v8_function_template_set_hidden_prototype(V8Handle self, bool value);
 bool v8_function_template_has_instance(V8Handle self, V8Handle object);
 
 /* ObjectTemplate */
 typedef V8Handle    (*V8AccessorGetter)(V8Handle, const V8AccessorInfo*);
 typedef void        (*V8AccessorSetter)(V8Handle, V8Handle, const V8AccessorInfo*);
 typedef V8AccessorGetter V8NamedPropertyGetter; 
-typedef V8AccessorSetter V8NamedPropertySetter;
+typedef V8Handle    (*V8NamedPropertySetter)(V8Handle, V8Handle, const V8AccessorInfo*);
 typedef V8Handle    (*V8IndexedPropertyGetter)(uint32_t, const V8AccessorInfo*);
 typedef V8Handle    (*V8IndexedPropertySetter)(uint32_t, V8Handle, const V8AccessorInfo*);
 
